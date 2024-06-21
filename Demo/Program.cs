1298b02bba677d3de6f0e44c1f88eddf1abeb0f9
Feras4Nayel <ferassaeednayel2@gmail.com>
2024-06-21 06:52:44 +0300

    Add project files.

---
+++ Demo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Id = 10;
+            Console.WriteLine(Id);
+        }
+    }
+}
