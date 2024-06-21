8cc45720705788a56c2c3f2fd3213ae61f81f5c4
Feras4Nayel <ferassaeednayel2@gmail.com>
2024-06-21 20:59:23 +0300

    Q-2

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -1,14 +1,38 @@
-﻿namespace Assignment_2
+﻿using System.ComponentModel;
+
+namespace Assignment_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             #region 1-	Write a program that allows the user to enter a number then print it.
-            Console.WriteLine("Please Enter a Number: ");
-            int number  = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(number);
+            //Console.WriteLine("Please Enter a Number: ");
+            //int number  = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(number);
             #endregion
+
+            #region  2 - Write C# program that Convert a string to an integer, but the string contains non-numeric characters.And mention what will happen 
+
+            //Console.WriteLine("Enter Input: ");
+            
+            //string Input = Console.ReadLine();
+
+            //int Output;
+
+            //if (int.TryParse(Input , out Output))
+            //{
+            //    Console.WriteLine($"The Result = {Output}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Can't Convert Non-numeric characters");
+            //}
+
+                    //If The String contains only numeric charachters it will be converted to integer
+                    //If The String contains non-numeric characters it will not be converted
+            #endregion
+
         }
     }
 }
